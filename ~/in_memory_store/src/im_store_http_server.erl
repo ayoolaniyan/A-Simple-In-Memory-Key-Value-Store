2715cf49959a10a407e7cb5b0812787f1eab5d0a
Olaniyan Omobolaji <info@ewebidentity.com>
2015-07-02 11:58:11 +0200

    HTTP Interface Update
    Uploaded HTTP Interface Updates

--- ~/in_memory_store/src/im_store_http_server.erl
+++ ~/in_memory_store/src/im_store_http_server.erl
@@ -2,59 +2,163 @@
 
 -behavior(e2_task).
 
--export([start_link/1]).
+-export([start_link/2, start_link/3]).
 
--export([init/0, handle_task/1, handle_post/1,get_content_length/1]).
+-export([init/0, handle_task/3, separate_path_parts/2, extract_params/1, send_html_message/2, send_plaintext_message/2, send_message/5]).
 
 
 	init() ->
 		Port = 1156,
 		e2_task:start_link(?MODULE, Port).
 		
-	start_link(Port) ->		
+	start_link(Port, Functions) ->
+		start_link(Port, Functions, null).
+		
+	start_link(Port, Functions, Pid) ->		
 		{ok, ListenSock} = gen_tcp:listen(Port, [list,{active, false}, {packet,http}]),
-		loop(ListenSock).
+		loop(ListenSock, Functions, Pid).
 	
-	loop(ListenSock) ->
+	loop(ListenSock, Functions, Pid) ->
 		io:format("server started.~n"),
 		{ok, Sock} = gen_tcp:accept(ListenSock),
-		spawn(?MODULE, handle_task, [Sock]),
-		loop(ListenSock).
+		spawn(?MODULE, handle_task, [Sock, Functions, Pid]),
+		loop(ListenSock, Functions, Pid).
 	
-	handle_task(Sock) ->
+	handle_task(Sock, Functions, Pid) ->
 		{ok, {http_request, Method, Path, Version}} = gen_tcp:recv(Sock, 0),
-			case (Method) of
-		'POST' -> handle_post(Sock);
-		_ -> send_unsupported_error(Sock)
-	end.
+		{abs_path,AbsPath}=Path,
+		{Headers, Body, PathString, QueryString, Params, Fragment}=marshall_request(Sock, Method, AbsPath),
+		Function=get_function(Method, Functions),
+			case Function of
+				error-> send_message(Sock, lists:flatten([atom_to_list(Method), " is not supported by this service."]), "text/plain", 501, "Not Implemented");
+			_->     case Pid of
+		    null -> Function(Sock, PathString, QueryString, Params, Fragment, Headers, Body);
+		    _  -> Function(Sock, PathString, QueryString, Params, Fragment, Headers, Body, Pid)
+		end
+    end,	
+    gen_tcp:close(Sock).
 	
-	get_content_length(Sock) ->
-		case gen_tcp:recv(Sock, 0, 60000) of
-			{ok, {http_header, _, 'Content-Length', _, Length}} -> list_to_integer(Length);
-			{ok, {http_header, _, Header, _, _}} -> get_content_length(Sock)
-	end.
 	
-	get_body(Sock, Length) ->
-		case gen_tcp:recv(Sock, 0) of
-			{ok, http_eoh} ->inet:setopts(Sock, [{packet, raw}]),{ok,Body}=gen_tcp:recv(Sock, Length),Body;
-		_ -> get_body(Sock, Length)
-	end.
-	
-	handle_post(Sock) ->
-		Length=get_content_length(Sock),
-		PostBody=get_body(Sock, Length),
-		io:fwrite(PostBody),
-		send_accept(Sock).
-			
-		
-%%%===================================================================
-%%% Internal Functions
-%%%===================================================================
-	
-	send_accept(Sock) ->
-		gen_tcp:send(Sock, "HTTP/1.1 202 Accepted\r\nConnection: close\r\nContent-Type: text/html; charset=UTF-8\r\nCache-Control: no-cache\r\n\r\n"),
-		gen_tcp:close(Sock).
+		send_html_message(Sock,Message)->
+		send_message(Sock, Message, "text/html", 200, "OK").
 
-	send_unsupported_error(Sock) ->
-		gen_tcp:send(Sock, "HTTP/1.1 405 Method Not Allowed\r\nConnection: close\r\nAllow: POST\r\nContent-Type: text/html; charset=UTF-8\r\nCache-Control: no-cache\r\n\r\n"),
-		gen_tcp:close(Sock).
+		send_plaintext_message(Sock,Message)->
+			send_message(Sock, Message, "text/plain", 200, "OK").
+
+		send_message(Sock, Message, ContentType, StatusCode, StatusDescription)->
+			case Message of
+			nil -> 
+				gen_tcp:send(Sock, lists:flatten(["HTTP/1.1 ", io_lib:format("~p ", [StatusCode]), StatusDescription, "\r\nContent-Type: ", ContentType, "; charset=UTF-8\r\nConnection: close\r\n\r\n"]));
+			_ ->
+				gen_tcp:send(Sock, lists:flatten(["HTTP/1.1 ", io_lib:format("~p ", [StatusCode]), StatusDescription, "\r\nContent-Type: ", ContentType, "; charset=UTF-8\r\nConnection: close\r\n\r\n", Message,"\r\n\r\n"]))
+			end.
+    
+
+
+				  
+		separate_path_parts(Path, Token)->
+			PartList=string:tokens(Path, Token),
+			[PathString|Part]=PartList,
+			{PathString,lists:flatten(Part)}.
+		 
+		handle_path(Path)->
+			{PathStringWithQuery, Fragment}=separate_path_parts(Path, "#"),
+			{PathString, QueryString}=separate_path_parts(Path, "?"),
+			Params=extract_params(QueryString),
+			{PathString, QueryString, Params, Fragment}.
+			
+
+		extract_params(QueryString)->    
+			Params=string:tokens(QueryString, "&"),
+			F=fun(X)->
+				  Tokens=string:tokens(X, "="),
+				  case length(Tokens) of
+				  2 ->
+					  [Name|[Value|_]]=Tokens,
+					  {Name, decode(re:replace(Value,"\\\+"," ",[global, {return,list}]))};
+				  _ -> lists:flatten(Tokens)
+				  end
+			  end,
+			lists:map(F, Params).
+				  
+						   
+		get_function(MethodAtom, [Head|Tail])->
+			case Head of
+			{MethodAtom, Function}->
+				Function;
+			_ ->get_function(MethodAtom, Tail)
+			end;
+		get_function(MethodAtom, []) ->
+			error.
+
+		marshall_request(Sock, Method, AbsPath) ->    
+			{Headers, Body}=headers(Sock, Method, []),
+			{PathString, QueryString, PathParams, Fragment}=handle_path(decode(AbsPath)),
+			ContentType=lists:keyfind('Content-Type', 1, Headers),
+			case ContentType of 
+			{_,"application/x-www-form-urlencoded"} ->BodyParams=extract_params(Body),{Headers, Body, PathString, QueryString, BodyParams, Fragment};
+			_->{Headers, Body, PathString, QueryString, PathParams, Fragment}
+			end.
+
+
+
+		headers(Sock, Method, Headers) ->
+			case gen_tcp:recv(Sock, 0) of	
+			{ok, http_eoh} -> body(Sock, Method, Headers);
+			{ok, {http_header,_,HeaderName,_, HeaderValue}} -> headers(Sock, Method, lists:append([{HeaderName, HeaderValue}], Headers))
+			end.
+
+		body(Sock, Method, Headers) ->
+			case (Method) of 
+			'GET' -> {Headers, []};
+			'POST' -> read_body(Sock, Headers);
+			'PUT' ->read_body(Sock, Headers);
+			'DELETE' -> {Headers, []}    
+			end.
+
+
+		read_body(Sock, Headers)->
+			inet:setopts(Sock, [{packet, raw}]),
+			ContentLength=lists:keyfind('Content-Length', 1, Headers),
+			
+			case ContentLength of
+				 false -> {Headers, []};
+			 {_,Length} -> LengthInt=list_to_integer(Length),
+						   case LengthInt of 
+							0 -> {Headers, []};
+						_ ->{ok,Body}=gen_tcp:recv(Sock, LengthInt),
+							{Headers, Body}
+						   end
+			end.
+
+
+
+		read_body(Sock, Headers, Count)->
+			inet:setopts(Sock, [{packet, raw}]),
+			case gen_tcp:recv(Sock, 0, 1000) of
+			{ok, Body} -> {Headers, Body};
+			{error, timeout}-> 
+				case Count of 
+				3 -> {error, "We No Habeas Corpus! (too many tries)"};
+				_ -> read_body(Sock, Headers, Count + 1)
+				end;
+			_ ->{error, "We No Habeas Corpus!"}
+			end.
+
+
+		%% taking this from inets/http_uri cause I can't seem to figure out import/export
+
+
+		decode(String) ->
+			do_decode(String).
+
+		do_decode([$%,Hex1,Hex2|Rest]) ->
+			[hex2dec(Hex1)*16+hex2dec(Hex2)|do_decode(Rest)];
+		do_decode([First|Rest]) ->
+			[First|do_decode(Rest)];
+		do_decode([]) ->
+			[].
+
+		hex2dec(X) when (X>=$0) andalso (X=<$9) -> X-$0;
+		hex2dec(X) when (X>=$A) andalso (X=<$F) -> X-$A+10;
+		hex2dec(X) when (X>=$a) andalso (X=<$f) -> X-$a+10.
